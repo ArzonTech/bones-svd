b35c154fc1385cd50af4c65fd57970b40e5402f8
ArzonTech <comandotimarketing@gmail.com>
2025-02-03 14:52:37 -0300

    Update cart.js

--- cart.js
+++ cart.js
@@ -111,7 +111,7 @@ document.addEventListener("DOMContentLoaded", () => {
             message += `- ${item.name} (x${item.quantity}) - R$ ${(item.price * item.quantity).toFixed(2)}\n`;
         });
         message += `\nTotal: R$ ${cart.reduce((sum, item) => sum + (item.price * item.quantity), 0).toFixed(2)}`;
-        const whatsappUrl = `https://wa.me/558487704819?text=${encodeURIComponent(message)}`;
+        const whatsappUrl = `https://wa.me/5584991123475?text=${encodeURIComponent(message)}`;
         window.open(whatsappUrl, "_blank");
     });
 });
